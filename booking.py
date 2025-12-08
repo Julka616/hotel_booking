50d47049e4b4cd8eb9341b54dd727e74d2a59cff
TwojeImię NazwiskoJulia <julia.lasota2002@wp.pl>
2025-12-09 00:23:08 +0100

    Add Booking class

--- booking.py
+++ booking.py
@@ -1,9 +1,7 @@
-from dataclasses import dataclass
-
-@dataclass
 class Booking:
-    id: int
-    room_id: int
-    guest_name: str
-    check_in: str  # ISO date YYYY-MM-DD
-    check_out: str
+    def __init__(self, id, room_id, user_name, start_date, end_date):
+        self.id = id
+        self.room_id = room_id
+        self.user_name = user_name
+        self.start_date = start_date
+        self.end_date = end_date
