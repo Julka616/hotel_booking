cb1a98a8a2a3ffd431c00dd982dc3c37c6f27647
TwojeImię NazwiskoJulia <julia.lasota2002@wp.pl>
2025-12-09 09:51:29 +0100

    Update Room class and add names to rooms.json

--- room.py
+++ room.py
@@ -1,7 +1,8 @@
 class Room:
-    def __init__(self, id, name, type, price, available=True):
+    def __init__(self, id, name, type, price, number=None, available=True):
         self.id = id
         self.name = name
         self.type = type
         self.price = price
+        self.number = number
         self.available = available
