f696ce73d7ceb7ac70b9e9597dde83b00cd6fa4b
TwojeImię NazwiskoJulia <julia.lasota2002@wp.pl>
2025-12-09 09:16:27 +0100

    Improve date validation in utils.py

--- utils.py
+++ utils.py
@@ -5,4 +5,5 @@ def validate_date(date_str):
         datetime.strptime(date_str, "%Y-%m-%d")
         return True
     except ValueError:
+        print("❌ Wrong date format. Use YYYY-MM-DD.")
         return False
